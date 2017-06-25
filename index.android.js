b94076facb6e6f6a8e1501dc4b0f661e2b6d07eb
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-06-24 23:56:30 -0500

    Creating first ui

--- index.android.js
+++ index.android.js
@@ -6,26 +6,100 @@ import {
   ScrollView,
   View
 } from 'react-native';
+import { Button, Card, Icon } from 'react-native-material-design';
 
 export default class MostroApp extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ flex: 0.5, backgroundColor: 'powderblue'}}>
-          <Text>Mostro Media</Text>
+        <View style={styles.nav}>
+          <Text style={styles.titleMostro}>Mostro Media</Text>
         </View>
-        <View style={{ flex: 4, backgroundColor: 'steelblue'}}>
-          <Text>Body</Text>
-        </View>
-        <View style={{ flex: 0.5, backgroundColor: 'powderblue'}}>
-          <Text>Footer</Text>
-        </View>                
+        <ScrollView style={{ flex: 4, backgroundColor: '#363636'}}>
+                <Card>
+                    <Card.Body>
+                        <Text>Some text to go in the body.</Text>
+                    </Card.Body>
+                    <Card.Actions position="right">
+                        <Button
+                          text='Button A' 
+                          value="ACTION" />
+                    </Card.Actions>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Text>Some text to go in the body.</Text>
+                    </Card.Body>
+                    <Card.Actions position="right">
+                        <Button
+                          text='Button A' 
+                          value="ACTION" />
+                    </Card.Actions>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Text>Some text to go in the body.</Text>
+                    </Card.Body>
+                    <Card.Actions position="right">
+                        <Button
+                          text='Button A' 
+                          value="ACTION" />
+                    </Card.Actions>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Text>Some text to go in the body.</Text>
+                    </Card.Body>
+                    <Card.Actions position="right">
+                        <Button
+                          text='Button A' 
+                          value="ACTION" />
+                    </Card.Actions>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Text>Some text to go in the body.</Text>
+                    </Card.Body>
+                    <Card.Actions position="right">
+                        <Button
+                          text='Button A' 
+                          value="ACTION" />
+                    </Card.Actions>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Text>Some text to go in the body.</Text>
+                    </Card.Body>
+                    <Card.Actions position="right">
+                        <Button
+                          text='Button A' 
+                          value="ACTION" />
+                    </Card.Actions>
+                </Card>                                                                                
+        </ScrollView>               
       </View>
     );
   }
 }
 
+
+
 const styles = StyleSheet.create({
+  nav:{
+    backgroundColor: '#E8540D',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 10,
+    elevation: 3
+  },
+  titleMostro:{
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
